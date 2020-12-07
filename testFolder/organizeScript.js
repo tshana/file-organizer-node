601ec97d71f258193c9de214fd4ae305bac5af1f
Taseen Shana <tshana@asu.edu>
2020-12-07 14:25:21 -0700

    Moving files to new folders works.
    Can move the list of specific files to the new organized folder.
    
    TODO:
    - Prevent overwriting an existing file in the organized folder when moving.
    - Change Misc array to ignore folders.

--- testFolder/organizeScript.js
+++ testFolder/organizeScript.js
@@ -4,17 +4,20 @@ const dir  = __dirname;
 
 const HTMLFolderName   = "HTML";
 const imagesFolderName = "Images";
-const moviesFolderName = "MOVIES";
+const videosFolderName = "Videos";
+const docsFolderName   = "Documents";
+const miscFolderName   = "Misc"; 
 
 // Should these be global objects ?
 const htmlFiles = [], jsFiles = [], jpgFiles = [], vidFiles = [], miscFiles = [];  
+const imageExtensionsArr = ['.jpg', '.png', '.gif', '.bmp', '.webp', '.svg'];
+const videoExtensionsArr = ['.webm', '.mov', '.mp4', '.flv', '.avi','.mkv', '.gifv', '.wmv' ];
 
-
-module.exports = () => {
+module.exports = async () => {
   console.log("Changing Files in Directory: " + dir + '\n');
 
-  // Scan the current directory for the files
-  fs.readdir(dir, {}, (err, files) => {
+  // Scans and lists the current directory for the files into seperate arrays.
+  await fs.readdir(dir, {}, (err, files) => {
     if(err) throw err;
 
     const fileList = files;
@@ -23,8 +26,9 @@ module.exports = () => {
       const extName = path.extname(fileName).toLocaleLowerCase();
       seperateFilestoArr(extName, fileName);
     })
-
     printExtensionObj()
+    createOrganizedFolders()
+    // moveFilesController()  //Un-comment when wanting to test file moving.
 
   })
 
@@ -36,10 +40,57 @@ function seperateFilestoArr(extentionType, fileName){
     case '.js':     jsFiles.push(fileName); break;
     case '.jpg':   jpgFiles.push(fileName); break;
     case '.mov':   vidFiles.push(fileName); break;
+    case '.png':   jpgFiles.push(fileName); break;
     default:      miscFiles.push(fileName);
   }
+
 }
 
+function seperateFilestoArrV2(extentionType, fileName){
+  if(imageExtensionsArr.includes(extentionType)){ jpgFiles.push(fileName); }
+  if(videoExtensionsArr.includes(extentionType)){ vidFiles.push(fileName); }
+}
+
+function moveFilesController(){
+  moveFiles(htmlFiles, HTMLFolderName)
+  moveFiles(jpgFiles,  imagesFolderName)
+  moveFiles(vidFiles,  videosFolderName)
+  // Misc includes folders in the list... Find a way to Ignore folders to make the test case work.
+  moveFiles(miscFiles, miscFolderName)
+}
+
+function moveFiles(fileListArray, folderName){
+
+  fileListArray.map((file)=>{
+    const filepathOrig = path.join(dir, file);
+    const folderPath   = path.join(dir, folderName);
+    const filePathNew  = path.join(folderPath, file);
+
+    fs.rename(filepathOrig, filePathNew,(err)=>{ if(err) throw err; })
+
+  })
+}
+
+function createOrganizedFolders(){
+  // Probably add a check to see if folders are already created, so errors don't occur when folders already exist. 
+
+  const htmlPath    = path.join(dir,HTMLFolderName);
+  const imagesPath  = path.join(dir,imagesFolderName);
+  const videosPath  = path.join(dir,videosFolderName);
+  const miscPath    = path.join(dir,miscFolderName);
+
+
+  fs.mkdir(htmlPath,  {},(err)=>{ if(err) throw err; })
+  fs.mkdir(imagesPath,{},(err)=>{ if(err) throw err; })
+  fs.mkdir(videosPath,{},(err)=>{ if(err) throw err; })
+
+  // Misc includes folders in the list... Find a way to Ignore folders to make the test case work.
+  // fs.mkdir(miscPath,  {},(err)=>{ if(err) throw err; })
+
+}
+
+
+// Helper Methods to show file list.
 function printList(type, fileArray){ 
   console.log(`${type} arr: ${JSON.stringify(fileArray)}`); 
 }
@@ -49,6 +100,4 @@ function printExtensionObj(){
   printList('Video', vidFiles);
   printList('JS',   jsFiles); 
   printList('MISC', miscFiles);
-
-
 }
